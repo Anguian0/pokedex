e6e2ef2b11561094f722ef80e2557537a375234a
Francisco <franciscoanguiano2000@gmail.com>
2022-06-20 18:56:09 -0500

    Iniciamos con Poke Api

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './componentes/Main';
+import './componentes/style.css'
+
+function App() {
+  return (
+    <>
+    <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <h2>1</h2>
+        <img src="pikachu.png" alt="" />
+        <h2>Pikachu</h2>
+      </div>
+    </>
+  );
+};
+export default Card;
