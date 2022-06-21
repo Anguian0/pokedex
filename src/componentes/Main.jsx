8460051ff9a772b96c6b26db9fc2ba79a7ca3a82
Francisco <franciscoanguiano2000@gmail.com>
2022-06-20 21:25:50 -0500

    Se resolvio el problema de duplicado en las cards

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -7,12 +7,31 @@ const Main = () => {
 
   const [pokeData, setPokeData] = useState([]);
   const [loading,setLoading] = useState(true);
-  const [url,setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [url,setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+  const [nextUrl,setNextUrl] = useState();
+  const [prevUrl,setPrevUrl] = useState();
+  const [pokeDex,setPokeDex] = useState();
 
   const pokeFun = async () => {
     setLoading(true)
     const res = await axios.get(url);
-    console.log(res.data)
+    // console.log(res.data.results)
+    setNextUrl(res.data.next);
+    setPrevUrl(res.data.previous);
+    getPokemon(res.data.results)
+    setLoading(false)
+  }
+
+  const getPokemon = async (res) => {
+    res.map(async(item) => {
+        const result = await axios.get(item.url)
+
+        setPokeData(state => {
+            state = [...state,result.data]
+            state.sort((a,b) => a.id>b.id?1:-1)
+            return state;
+        })
+    })
   }
 
   useEffect (() => {
@@ -23,17 +42,14 @@ const Main = () => {
     <>
       <div className="container">
         <div className="left-content">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card pokemon={pokeData} loading={loading} infoPokemon={poke=>setPokeDex(poke)}/>
           <div className="btn-group">
             <button>Atras</button>
             <button>Siguiente</button>
           </div>
         </div>
         <div className="right-content">
-          <Pokeinfo />
+          <Pokeinfo data={pokeDex}/>
         </div>
       </div>
     </>
